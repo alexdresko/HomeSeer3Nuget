0d66e984a443620d8587c14cdb28e3d96ba8dba9
Alex Dresko <me@alexdresko.com>
2018-10-28 18:35:41 -0400

    update nuspec

---
+++ update-nuspec.ps1
@@ -0,0 +1,21 @@
+﻿Param(
+  [string]$newVersion
+)
+
+$file = ".\HomeSeerNuget\HomeSeerNuget.nuspec"
+
+[xml]$xml = get-content -Path $file
+
+if ($newVersion -eq "") {
+    $version = [System.Version]::Parse($xml.package.metadata.version)
+
+    $newVersion = "$($version.Major).$($version.Minor).$($version.Build + 1)"
+} else {
+    $newVersion = $newVersion -replace "(\d*\.\d*\.\d*).*", "`$1"
+}
+
+$xml.package.metadata.version = $newVersion
+
+$xml.Save($file)
+
+"Updated $file to $newVersion "
